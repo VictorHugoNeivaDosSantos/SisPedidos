117fe73d363bb1c9af7c339baed7c42ba0016ea0
VictorHugoNeivaDosSantos <victorhugoneiva2017@gmail.com>
2021-10-01 06:59:33 -0300

    Implementar autenticação

--- SisPedidos/Controllers/TokenController.cs
+++ SisPedidos/Controllers/TokenController.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SisPedidos.Model;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SisPedidos.Controllers
@@ -14,8 +19,24 @@ namespace SisPedidos.Controllers
 
         [HttpPost]
 
-        public IActionResult GetToken()
+        public IActionResult GetToken([FromBody] User user)
         {
+            if (user.UserName == "teste" && user.Password == "12345")
+            {
+                var claims = new[]
+                {
+                      new Claim(ClaimTypes.Name, user.UserName)
+                };
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
+                var credts = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                token = new JwtSecurityTokenHandler().WriteToken(token);
+
+            }
+
+
+
+
 
         }
     }
